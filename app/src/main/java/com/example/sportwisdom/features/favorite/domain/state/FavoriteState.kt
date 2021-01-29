0074310470ff5d34aa62ac17d7cee9756f590052
brunohensel <bruno.hensel@gmail.com>
2021-01-29 16:15:24 +0100

    implemented FavoriteReducer.kt

--- app/src/main/java/com/example/sportwisdom/features/favorite/domain/state/FavoriteState.kt
+++ app/src/main/java/com/example/sportwisdom/features/favorite/domain/state/FavoriteState.kt
@@ -1,4 +1,17 @@
 package com.example.sportwisdom.features.favorite.domain.state
 
-class FavoriteState {
+import com.example.sportwisdom.features.search.domain.model.TeamDto
+import kotlinx.coroutines.flow.Flow
+
+data class FavoriteState(
+  val favoriteModel: Flow<List<TeamDto>>,
+  val syncState: FavoriteSyncState
+)
+
+sealed class FavoriteSyncState {
+  object Loading : FavoriteSyncState()
+  object Content : FavoriteSyncState()
+  object SideEffect : FavoriteSyncState()
+  object Empty : FavoriteSyncState()
+  data class Message(val msg: String?) : FavoriteSyncState()
 }
